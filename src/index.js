58da80353baa6aa7a644ed420976016971b63d8b
Axel Chalon <chalon.axel@gmail.com>
2018-03-28 16:41:48 +0200

    set up router

--- src/index.js
+++ src/index.js
@@ -14,19 +14,79 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
+
+import es6Promise from 'es6-promise';
+
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+ 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { hashHistory } from 'react-router';
+
+import Api from '@parity/api';
+
+// import ContractInstances from '~/contracts';
+
+// import { initStore } from './redux';
 import ContextProvider from '@parity/ui/lib/ContextProvider';
+// import muiTheme from '@parity/ui/lib/Theme';
+import MainApplication from './main';
 
-import api from './api';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
+// import { loadSender, patchApi } from '~/util/tx';
+// import { setApi } from '~/redux/providers/apiActions';
 
-/*ReactDOM.render(
-  <ContextProvider api={api}>
-    <App />
-  </ContextProvider>,
+// import './environment';
+
+// @TODO REDESIGN
+// import '../assets/fonts/Roboto/font.css';
+// import '../assets/fonts/RobotoMono/font.css';
+
+
+
+es6Promise.polyfill();
+
+injectTapEventPlugin();
+
+const api = new Api(window.ethereum);
+
+// patchApi(api); @TODO
+// loadSender(api);
+// ContractInstances.create(api);
+
+// const store = initStore(api, hashHistory);
+// store.dispatch({ type: 'initAll', api });
+// store.dispatch(setApi(api));
+
+// store={ store }
+// muiTheme={ muiTheme }
+// muiTheme={ muiTheme }
+
+ReactDOM.render(
+  <AppContainer>
+    <ContextProvider api={ api } >
+      <MainApplication
+        routerHistory={ hashHistory }
+      />
+    </ContextProvider>
+  </AppContainer>,
   document.querySelector('#root')
-);*/
-// registerServiceWorker();
+);
+
+if (module.hot) {
+  module.hot.accept('./main.js', () => {
+    require('./main.js');
+// store={ store }
+    ReactDOM.render(
+      <AppContainer>
+        <ContextProvider api={ api } >
+          <MainApplication
+            routerHistory={ hashHistory }
+          />
+        </ContextProvider>
+      </AppContainer>,
+      document.querySelector('#root')
+    );
+  });
+}
