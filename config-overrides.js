abd80d47d3a70ad49aee7d400f49074fd8be6e8a
Axel Chalon <chalon.axel@gmail.com>
2018-04-17 15:43:39 +0200

    Update parity-react-app for inject.js injection

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,9 @@ const path = require('path');
 const rewireParity = require('react-app-rewire-parity');
 
 module.exports = (config) => {
-  config = rewireParity(config);
+  config = rewireParity(config, {
+    htmlScriptUris: [ '/parity-utils/inject.js' ]
+  });
 
   config.resolve.alias['~'] = path.resolve(__dirname, 'src/');
 
