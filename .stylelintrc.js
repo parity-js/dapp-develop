ba70a39c5bf849071d1145d2ebbbd77eea5886ba
Axel Chalon <chalon.axel@gmail.com>
2018-04-25 20:58:56 +0200

    Linting

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,1 +1,5 @@
-module.exports = require('stylelint-config-parity');
+var cfg = require('stylelint-config-parity');
+cfg['rules']['block-no-empty'] = null;
+cfg['rules']['selector-list-comma-newline-after'] = null;
+
+module.exports = cfg;
